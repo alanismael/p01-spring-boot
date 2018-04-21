3490ffcf66eee8edceec1711a25cc662c97c4e63
unknown <allanismael@gmail.com>
2018-04-20 23:55:51 -0300

    Segundo commit

--- src/main/java/tjma/jus/viagem/ViagemApplication.java
+++ src/main/java/tjma/jus/viagem/ViagemApplication.java
@@ -8,5 +8,6 @@ public class ViagemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ViagemApplication.class, args);
+        System.out.println();
     }
 }
