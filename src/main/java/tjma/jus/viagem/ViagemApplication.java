b7a5b54c7cba7845f301068ef9ebcd31fa7b8fc2
unknown <allanismael@gmail.com>
2018-04-29 20:41:15 -0300

    Flyway

--- src/main/java/tjma/jus/viagem/ViagemApplication.java
+++ src/main/java/tjma/jus/viagem/ViagemApplication.java
@@ -8,6 +8,6 @@ public class ViagemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ViagemApplication.class, args);
-        System.out.println("Iniciando...");
+        System.out.println("Iniciando o sistema...");
     }
 }
